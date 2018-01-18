38637e1857e0a1cdf84d74a549f3afcb0f1da2a6
Levi Fuller <lfuller941@gmail.com>
2018-01-17 22:17:06 -0700

    Material UI working and movie-browser container is scaffolded;

--- src/modules/movie-browser/movie-browser.container.js
+++ src/modules/movie-browser/movie-browser.container.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {Grid, Row, Col} from 'react-bootstrap';
+import {AppBar} from 'material-ui';
 
 class MovieBrowser extends React.Component {
   render() {
-    <div>
-      <h1>Welcome </h1>
-    </div>
+    return (
+      <div>
+        <AppBar title='Movie Browser' />
+        <Grid>
+          <Row>
+            <p>Search will go here</p>
+          </Row>
+          <Row>
+            <p>Movie list will go here</p>
+          </Row>
+        </Grid>
+      </div>
+    );
   }
 }
 
