d34fa23fb352fc8baf45c4c36a5338aca02ba64a
Levi Fuller <lfuller941@gmail.com>
2018-01-17 22:24:07 -0700

    Comments on thunk/logger middleware

--- src/store.js
+++ src/store.js
@@ -11,11 +11,20 @@ const rootReducer = combineReducers({
 const loggerMiddleware = createLogger();
 
 const store = createStore(
+  // The root reducer will serve as the parent for all other reducers
   rootReducer,
+  // The initial state (will be populated by the initial state of our other reducers so it can be undefined)
   undefined,
+  // compose simply enables us to apply several store enhancers
+  // Right now, we are only using applyMiddlware, but this could change in the future
   compose(
     applyMiddleware(
+      // Thunk allows functions to be returned from action creators
+      // so we can do things like dispatch multiple actions in a 
+      // single action creator for async actions
       thunkMiddlware,
+      // Will log information about dispatched actions to the console
+      // including the previous state, the action details, and the next state
       loggerMiddleware
     )
     // DevTools.instrument()
